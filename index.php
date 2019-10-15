b522a33f362838e5ddb8c98383b8b4ba1b198c0f
HiMBixD <tuananvip123@gmail.com>
2019-10-15 11:52:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -27,25 +27,8 @@
 $result = query1($query);
       foreach ($result as $row) {
 
-          ?>
-          <div class="col-md-3"style="max-width:100%;width: 20%;margin:0px;">
-
-              <form method="post" action="Shoppingcart.php?action=add&ProductID=<?php echo $row["ProductID"]; ?>" style=" position: relative;display:inline-block;max-width:100%;">
-
-                  <div class="product"style="float:left;">
-                      <img src="<?php echo $row["ProductImg"]; ?>" class="img-responsive"style="max-width:100%;">
-                        <a href="./Showproduct.php?ProductID=<?php echo $row["ProductID"]?>"><h5 class="text-info"><?php echo $row["ProductName"]; ?></h5></a>
-                      <h5 class="text-danger"><?php echo "$".$row["ProductPrice"]; ?></h5>
-                      <input type="text" name="productquantity" class="form-control" value="1">
-                      <input type="hidden" name="productname" value="<?php echo $row["ProductName"]; ?>">
-                      <input type="hidden" name="productprice" value="<?php echo $row["ProductPrice"]; ?>">
-                      <input type="submit" name="add" style="margin-top: 5px;" class="btn btn-success"
-                             value="Add to Cart">
-                  </div>
-              </form>
-          </div>
-          <?php
+          echo "<li>". $row["productid"] . '--' . $row["productname"] . '--' . $row["productprice"] . '--' . $row["productimg"] . '--' . $row["productquantity"] . '--' . $row["productdescription"] . "</li>";
       }
       ?>
       </div>
-      <?php
+      
