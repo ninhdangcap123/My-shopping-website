6ee80d9f4d959d1feca4299cac0cb46e03254f18
Theplayer <nguyenhaininh333@gmail.com>
2019-10-15 10:17:43 +0700

    Update Function.php

--- Function.php
+++ Function.php
@@ -1,101 +1,10 @@
 <?php
-
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "my website";
-$port="3306";
-
-function query($sql){
-
-  global $servername ;
-  global $username;
-  global $password;
-  global $dbname;
-  global $port;
-  $conn = new mysqli($servername, $username, $password, $dbname,$port);
-  // Check connection
-  if ($conn->connect_error) {
-      die("Connection failed: " . $conn->connect_error);
-  }
-
-//the result get value from sql
-  $result=$conn->query($sql);
-  $rows=mysqli_fetch_all($result);
-  return $rows;
+$con = pg_connect("host = ec2-54-83-33-14.compute-1.amazonaws.com , dbname = daaa8gq0g2jfmc , user =  uzyjkzbzfuukqf , servername = My-shopping-website , port = 5432");
+if (!$con){
+  echo "not connect";
 
 }
-
-function insert($sql){
-
-  global $servername ;
-  global $username;
-  global $password;
-  global $dbname;
-  global $port;
-  $conn1 = new mysqli($servername, $username, $password, $dbname,$port);
-  // Check connection
-  if ($conn1->connect_error) {
-      die("Connection failed: " . $conn1->connect_error);
-  }
-
-  if (mysqli_query($conn1,$sql))
-   {
-
-  }
-  else {
-    echo "Error:".$sql."<br>".mysqli_error($conn1);
-  }
-
-
+else{
+  echo"connect";
 }
-function update($sql){
-
-  global $servername ;
-  global $username;
-  global $password;
-  global $dbname;
-  global $port;
-  $conn1 = new mysqli($servername, $username, $password, $dbname,$port);
-  // Check connection
-  if ($conn1->connect_error) {
-      die("Connection failed: " . $conn1->connect_error);
-  }
-
-  if (mysqli_query($conn1,$sql))
-   {
-
-  }
-  else {
-    echo "Error:".$sql."<br>".mysqli_error($conn1);
-  }
-
-
-}
-function delete($sql){
-
-  global $servername ;
-  global $username;
-  global $password;
-  global $dbname;
-  global $port;
-  $conn1 = new mysqli($servername, $username, $password, $dbname,$port);
-  // Check connection
-  if ($conn1->connect_error) {
-      die("Connection failed: " . $conn1->connect_error);
-  }
-
-  if (mysqli_query($conn1,$sql))
-   {
-
-  }
-  else {
-    echo "Error:".$sql."<br>".mysqli_error($conn1);
-  }
-
-
-}
-
-
-
  ?>
