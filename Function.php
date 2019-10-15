1ce2ba0adefbf384ef161fcbcae9c2d9c9b8c3a6
Theplayer <nguyenhaininh333@gmail.com>
2019-10-15 10:49:09 +0700

    Update Function.php

--- Function.php
+++ Function.php
@@ -1,10 +1,10 @@
 <?php
-$con = pg_connect("host = ec2-54-83-33-14.compute-1.amazonaws.com , dbname = daaa8gq0g2jfmc , username =  uzyjkzbzfuukqf , password = 38aa430fd2fd2563a7b0165d8ce02a3bdae65c9884efdc6bb05f013fc82b5403 , port = 5432");
-if (!$con){
-  echo "not connect";
+$con = " dbname  = daaa8gq0g2jfmc host = ec2-54-83-33-14.compute-1.amazonaws.com, user =uzyjkzbzfuukqf, password = 38aa430fd2fd2563a7b0165d8ce02a3bdae65c9884efdc6bb05f013fc82b5403 , sslmode= require ":
+if ( $con ){
+  echo "connect";
 
 }
-else{
-  echo"connect";
+else {
+  echo" not connect";
 }
  ?>
