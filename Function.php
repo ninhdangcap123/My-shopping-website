42299a1d2579b751acec2e1b1b7a9282d49341fa
Theplayer <nguyenhaininh333@gmail.com>
2019-10-15 10:20:35 +0700

    Update Function.php

--- Function.php
+++ Function.php
@@ -1,5 +1,5 @@
 <?php
-$con = pg_connect("host = ec2-54-83-33-14.compute-1.amazonaws.com , dbname = daaa8gq0g2jfmc , user =  uzyjkzbzfuukqf , servername = My-shopping-website , port = 5432");
+$con = pg_connect("host = ec2-54-83-33-14.compute-1.amazonaws.com , dbname = daaa8gq0g2jfmc , username =  uzyjkzbzfuukqf , password = 38aa430fd2fd2563a7b0165d8ce02a3bdae65c9884efdc6bb05f013fc82b5403 , port = 5432");
 if (!$con){
   echo "not connect";
 
