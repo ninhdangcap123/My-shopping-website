8c684da5e8ad577cf1ed1e50124b87b5205ef9bf
Theplayer <nguyenhaininh333@gmail.com>
2019-10-21 17:11:09 +0700

    test

--- Delete_Product_action_page.php
+++ Delete_Product_action_page.php
@@ -4,10 +4,10 @@ require_once './Function.php';
 if (isset($_GET['ProductID'])) {
   $ProductIDx= $_GET['ProductID'];
 $sql="DELETE from product where ProductID=$ProductIDx";
-delete($sql);
 
 
-if (!delete($sql)) {
+
+if (delete($sql)) {
   echo '<script>alert("Delete failed!")</script>';
   header("location: Delete.php");
 }
