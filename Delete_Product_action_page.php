f78f833233cfb2513234e89f11eaff36557aee94
HiMBixD <tuananvip123@gmail.com>
2019-10-23 14:54:30 +0700

    Update Delete.php

--- Delete_Product_action_page.php
+++ Delete_Product_action_page.php
@@ -1,20 +1,5 @@
 
 <?php
-require_once './Function.php';
-if (isset($_GET['ProductID'])) {
-  $ProductIDx= $_GET['ProductID'];
-$sql="DELETE from product where ProductID=$ProductIDx";
 
-
-
-if (delete($sql)) {
-  echo '<script>alert("Delete failed!")</script>';
-  header("location: Delete.php");
-}
-else {
-  echo '<script>alert("Delete success!")</script>';
-  header("location: Delete.php");
-}
-
-}
+echo "abc";
  ?>
