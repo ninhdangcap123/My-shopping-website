3c264d2ea963f9cba67abcaf5235b58c58ffc3eb
Theplayer <nguyenhaininh333@gmail.com>
2019-10-23 14:56:04 +0700

    test

--- Delete_Product_action_page.php
+++ Delete_Product_action_page.php
@@ -1,5 +1,21 @@
 
 <?php
 
-echo "abc";
+require_once './Function.php';
+if (isset($_POST['ProductID'])) {
+  $ProductIDx= $_POST['ProductID'];
+$sql="DELETE from product where ProductID=$ProductIDx";
+
+
+
+if (delete($sql)) {
+  echo '<script>alert("Delete failed!")</script>';
+  header("location: Delete.php");
+}
+else {
+  echo '<script>alert("Delete success!")</script>';
+  header("location: Delete.php");
+}
+
+}
  ?>
