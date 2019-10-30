62adc413dbfb8be20eb537c7c45325dc72330042
HiMBixD <tuananvip123@gmail.com>
2019-10-30 13:57:49 +0700

    Update Delete_Product_action_page.php

--- Delete_Product_action_page.php
+++ Delete_Product_action_page.php
@@ -4,12 +4,13 @@
 require_once './Function.php';
 if (isset($_GET['ProductID'])) {
   $ProductIDx= $_GET['ProductID'];
-$sql="DELETE from product where ProductID=$ProductIDx";
+$sql="DELETE FROM product WHERE ProductID=$ProductIDx";
 $resultx=delete($sql);
 
 
 if ($resultx==5) {
   echo '<script>alert("Delete failed!")</script>';
+  echo "$ProductIDx";
   header("refresh:1; url=Delete.php");
 }
 
